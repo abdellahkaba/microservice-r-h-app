91661a9f2f6331810d24b584a146a50f14cb1ea0
Abdellah <abdallahkaba98@gmail.com>
2024-05-26 02:48:06 +0000

    dockerisation-authService

---
+++ authenticate-service/src/main/java/com/kaba/authenticateservice/AuthenticateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kaba.authenticateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticateServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticateServiceApplication.class, args);
+    }
+
+}
