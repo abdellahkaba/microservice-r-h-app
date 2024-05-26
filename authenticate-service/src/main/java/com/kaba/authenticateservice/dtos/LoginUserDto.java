39c24b6011fb85c6933f9e6ac95a41c4b50f15a3
Abdellah <abdallahkaba98@gmail.com>
2024-05-26 18:14:51 +0000

    RegisterDto-LoginDto

---
+++ authenticate-service/src/main/java/com/kaba/authenticateservice/dtos/LoginUserDto.java
@@ -0,0 +1,16 @@
+package com.kaba.authenticateservice.dtos;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginUserDto {
+    private String email;
+    private String password;
+}
---
+++ authenticate-service/src/main/java/com/kaba/authenticateservice/dtos/RegisterUserDto.java
@@ -0,0 +1,16 @@
+package com.kaba.authenticateservice.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterUserDto {
+    private String email;
+    private String password;
+    private String fullName;
+}
