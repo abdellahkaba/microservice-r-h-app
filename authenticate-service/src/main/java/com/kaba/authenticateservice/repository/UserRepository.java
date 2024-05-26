1946438a6ce8ed1c206274b10ca66ea4a8c9b359
Abdellah <abdallahkaba98@gmail.com>
2024-05-26 16:36:31 +0000

    create-user-up

---
+++ authenticate-service/src/main/java/com/kaba/authenticateservice/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.kaba.authenticateservice.repository;
+
+import com.kaba.authenticateservice.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface UserRepository extends CrudRepository<User,Integer> {
+    Optional<User> findByEmail(String email);
+}
